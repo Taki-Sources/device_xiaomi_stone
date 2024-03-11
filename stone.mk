e7a685738661484b3e8a99774688d1762939ba8e
Taki / <amrito@myyahoo.com>
2024-03-11 10:47:57 +0530

    stone: Bringup For Zephyrus
    * Bring Up Device Tree According To Zephyrus
    * Added Product & Rom Specific Flags
    * Added Maintainer Information
    
    Co-Authored-By: Ivan Martynov <johnivan19999@gmail.com>

--- stone.mk
+++ stone.mk
@@ -8,9 +8,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from stone device
 $(call inherit-product, device/xiaomi/stone/device.mk)
 
@@ -21,14 +18,23 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_DISABLE_EPPE := true
 TARGET_DEBLOAT := true
 
+# Product Flags
+PRODUCT_BOARD_PLATFORM := holi
+PRODUCT_USES_QCOM_HARDWARE := true
+
+# Rom Specific Flags
+CUSTOM_BUILD_TYPE := Official
+GMS_FULL := true
+WITH_GMS := true
+
 # Charging Animation
-TARGET_USE_PIXEL_CHARGER := true
+USE_PIXEL_CHARGER_IMAGES := true
 
 # Use Prebuilt Kernel
 PREBUILT_KERNEL := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_stone
+PRODUCT_NAME := stone
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
-git clone https://github.com/Amrito-Projects/device_xiaomi_sm6375-common.git -b lineage-21-p device/xiaomi/sm6375-common
+git clone https://github.com/Amrito-Projects/device_xiaomi_sm6375-common.git -b zeph device/xiaomi/sm6375-common
 
 git clone --depth=1 https://github.com/Amrito-Projects/vendor_xiaomi.git -b lineage-21 vendor/xiaomi
 
 git clone --depth=1 https://github.com/Amrito-Projects/device_xiaomi_stone-kernel.git -b 13-p device/xiaomi/stone-kernel
 
-git clone --depth=1 https://github.com/Stone-Trees/hardware_xiaomi.git hardware/xiaomi
+git clone --depth=1 https://github.com/Stone-Trees/hardware_xiaomi.git vendor/hardware/xiaomi
 
 git clone --depth=1 https://github.com/Drpto/packages_apps_prebuilts.git packages/apps/prebuilts
